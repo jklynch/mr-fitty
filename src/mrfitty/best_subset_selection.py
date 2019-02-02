99c82ffd2e28959aaa809edb1ac3bafe4ea45abf
Joshua Lynch <joshua.kevin.lynch@gmail.com>
2019-02-01 21:53:02 -0500

    move prediction error calculation to new method fit_and_predict

--- src/mrfitty/best_subset_selection.py
+++ src/mrfitty/best_subset_selection.py
@@ -21,6 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+from collections import defaultdict
 import itertools
 import logging
 import time
@@ -36,7 +37,7 @@ from mrfitty.linear_model import NonNegativeLinearRegression
 
 
 def pairwise(iterable):
-    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    """s -> (s0,s1), (s1,s2), (s2, s3), ..."""
     a, b = itertools.tee(iterable)
     next(b, None)
     return zip(a, b)
@@ -57,18 +58,29 @@ class BestSubsetSelectionFitTask(AllCombinationFitTask):
         )
 
     def get_fit_quality_score_text(self, any_given_fit):
-        return ' (median C_p: {:8.5f} <-- {:8.5f} --> {:8.5f})'.format(
-            any_given_fit.median_C_p_ci_lo,
-            any_given_fit.median_C_p,
-            any_given_fit.median_C_p_ci_hi)
+        return [
+            'MSPE 95% ci of median: {:.5f} <-- {:.5f} --> {:.5f}'.format(
+                any_given_fit.median_C_p_ci_lo,
+                any_given_fit.median_C_p,
+                any_given_fit.median_C_p_ci_hi
+            ),
+            'MSE: {:<8.5f}'.format(any_given_fit.nss)
+        ]
 
     def choose_best_component_count(self, all_counts_spectrum_fit_table):
         """
-        Calculate the prediction error for top fits.
+        Calculate the prediction error statistics for top fits.
 
-        :param all_counts_spectrum_fit_table:
+        Parameters
+        ----------
+        all_counts_spectrum_fit_table (dict)
           dictionary with component count keys, sorted list of spectrum fit list values
-        :return:
+
+        Returns
+        -------
+        best_fit (SpectrumFit)
+            the fit with having lowest 95% confidence interval of median prediction error AND lowest number
+            of reference components
         """
         log = logging.getLogger(__name__)
 
@@ -82,11 +94,17 @@ class BestSubsetSelectionFitTask(AllCombinationFitTask):
             for component_count in all_counts_spectrum_fit_table.keys()
         }
 
+        all_counts_spectrum_fit_pe_table = defaultdict(list)
         for component_count_i in sorted(all_counts_spectrum_fit_table.keys()):
             # calculate C_p for the first ? fits with component_count_i
             log.debug('calculating CI of median C_p for {} component(s)'.format(component_count_i))
 
-            for fit_j in all_counts_spectrum_fit_table[component_count_i]:
+            sorted_fits_for_i_components = sorted(
+                all_counts_spectrum_fit_table[component_count_i],
+                key=lambda fit: fit.nss
+            )
+
+            for fit_j in sorted_fits_for_i_components[:20]:
                 t0 = time.time()
                 prediction_errors, _ = self.calculate_prediction_error_list(fit_j)
                 t1 = time.time()
@@ -100,11 +118,13 @@ class BestSubsetSelectionFitTask(AllCombinationFitTask):
 
                 fit_j.prediction_errors = prediction_errors
 
+                all_counts_spectrum_fit_pe_table[component_count_i].append(fit_j)
                 #log.info('%5.2fs to calculate prediction error list', t1 - t0)
 
-            all_counts_spectrum_fit_table[component_count_i] = sorted(
-                all_counts_spectrum_fit_table[component_count_i],
-                key=lambda fit: (fit.median_C_p, fit.median_C_p_ci_lo, fit.median_C_p_ci_hi))
+            all_counts_spectrum_fit_pe_table[component_count_i] = sorted(
+                all_counts_spectrum_fit_pe_table[component_count_i],
+                key=lambda fit: (fit.median_C_p, fit.median_C_p_ci_lo, fit.median_C_p_ci_hi)
+            )
             # for each fit find all fits with overlapping ci
             # 4 cases:
             #
@@ -125,11 +145,13 @@ class BestSubsetSelectionFitTask(AllCombinationFitTask):
             #
             #                         <-- k -->             j.hi < k.lo (stop checking)
             #              <-- j -->
-            for fit_j in all_counts_spectrum_fit_table[component_count_i]:
-                for fit_k in all_counts_spectrum_fit_table[component_count_i]:
+            for fit_j in all_counts_spectrum_fit_pe_table[component_count_i]:
+                for fit_k in all_counts_spectrum_fit_pe_table[component_count_i]:
                     if fit_j == fit_k:
-                        log.info('* component count %d: %8.5f <-- %8.5f --> %8.5f',
-                            component_count_i, fit_j.median_C_p_ci_lo, fit_j.median_C_p, fit_j.median_C_p_ci_hi)
+                        log.info(
+                            '* component count %d: %8.5f <-- %8.5f --> %8.5f',
+                            component_count_i, fit_j.median_C_p_ci_lo, fit_j.median_C_p, fit_j.median_C_p_ci_hi
+                        )
                     elif fit_j.median_C_p_ci_hi < fit_k.median_C_p_ci_lo:
                         # assuming later fits will not overlap with fit_j
                         break
@@ -137,38 +159,37 @@ class BestSubsetSelectionFitTask(AllCombinationFitTask):
                         # assuming later fits could overlap with fit_j
                         pass
                     elif fit_j.median_C_p_ci_lo <= fit_k.median_C_p_ci_lo <= fit_j.median_C_p_ci_hi:
-                        log.info('  component count %d: %8.5f <-- %8.5f --> %8.5f',
-                                 component_count_i, fit_k.median_C_p_ci_lo, fit_k.median_C_p, fit_k.median_C_p_ci_hi)
+                        log.info(
+                            '  component count %d: %8.5f <-- %8.5f --> %8.5f',
+                            component_count_i, fit_k.median_C_p_ci_lo, fit_k.median_C_p, fit_k.median_C_p_ci_hi
+                        )
                     elif fit_j.median_C_p_ci_lo <= fit_k.median_C_p_ci_hi <= fit_j.median_C_p_ci_hi:
-                        log.info('  component count %d: %8.5f <-- %8.5f --> %8.5f',
-                                 component_count_i, fit_k.median_C_p_ci_lo, fit_k.median_C_p, fit_k.median_C_p_ci_hi)
+                        log.info(
+                            '  component count %d: %8.5f <-- %8.5f --> %8.5f',
+                            component_count_i, fit_k.median_C_p_ci_lo, fit_k.median_C_p, fit_k.median_C_p_ci_hi
+                        )
                     else:
-                        log.info('  component count %d: %8.5f <-- %8.5f --> %8.5f',
-                                 component_count_i, fit_k.median_C_p_ci_lo, fit_k.median_C_p, fit_k.median_C_p_ci_hi)
+                        log.info(
+                            '  component count %d: %8.5f <-- %8.5f --> %8.5f',
+                            component_count_i, fit_k.median_C_p_ci_lo, fit_k.median_C_p, fit_k.median_C_p_ci_hi
+                        )
                 log.info('***')
 
-            best_fit_for_component_count = all_counts_spectrum_fit_table[component_count_i][0]
-            #t0 = time.time()
-            #prediction_errors, _ = self.calculate_prediction_error_list(best_fit_for_component_count)
-            #t1 = time.time()
-            #log.info('%5.2fs to calculate prediction error list', t1-t0)
+            best_fit_for_component_count = all_counts_spectrum_fit_pe_table[component_count_i][0]
 
             component_count_to_median_cp[component_count_i] = best_fit_for_component_count.median_C_p
             component_count_to_median_cp_ci_lo_hi[component_count_i] = (
                 best_fit_for_component_count.median_C_p_ci_lo,
                 best_fit_for_component_count.median_C_p_ci_hi
             )
-            #component_count_to_median_cp[component_count_i] = np.median(prediction_errors)
-            #component_count_to_median_cp_ci_lo_hi[component_count_i] = scikits.bootstrap.ci(
-            #    data=prediction_errors,
-            #    statfunction=np.median)
 
         log.debug('component count to median cp: {}'.format(component_count_to_median_cp))
         log.debug('component count to median cp confidence interval: {}'.format(component_count_to_median_cp_ci_lo_hi))
 
         best_component_count, C_p_lo, C_p_hi = self.get_best_ci_component_count(
             component_count_to_median_cp,
-            component_count_to_median_cp_ci_lo_hi)
+            component_count_to_median_cp_ci_lo_hi
+        )
         best_fit = all_counts_spectrum_fit_table[best_component_count][0]
         log.info('best fit: {}'.format(best_fit))
         return best_fit
@@ -224,7 +245,29 @@ class BestSubsetSelectionFitTask(AllCombinationFitTask):
         """
 
         normalized_C_p_list = []
-        model_residuals = np.full(shape=(fit.reference_spectra_A_df.shape[0], n_splits), fill_value=np.nan, dtype=np.double)
+        model_residuals = np.full(
+            shape=(fit.reference_spectra_A_df.shape[0], n_splits),
+            fill_value=np.nan,
+            dtype=np.double
+        )
+        #cv = sklearn.model_selection.ShuffleSplit(n_splits=n_splits, test_size=test_size)
+        #for i, (train_index, test_index) in enumerate(cv.split(fit.reference_spectra_A_df.values)):
+        #    lm = self.ls()
+        #    lm.fit(
+        #        fit.reference_spectra_A_df.values[train_index],
+        #        fit.unknown_spectrum_b.values[train_index]
+        #    )
+        #    predicted_b = lm.predict(fit.reference_spectra_A_df.values[test_index])
+        for i, (predicted_b, train_index, test_index) \
+                in enumerate(self.fit_and_predict(fit, n_splits=n_splits, test_size=test_size)):
+
+            model_residuals[test_index, i] = fit.unknown_spectrum_b.values[test_index] - predicted_b
+            cp = np.sqrt(np.nansum(np.square(model_residuals[test_index, i])))
+            normalized_cp = cp / len(test_index)
+            normalized_C_p_list.append(normalized_cp)
+        return normalized_C_p_list, model_residuals
+
+    def fit_and_predict(self, fit, n_splits=1000, test_size=0.2):
         cv = sklearn.model_selection.ShuffleSplit(n_splits=n_splits, test_size=test_size)
         for i, (train_index, test_index) in enumerate(cv.split(fit.reference_spectra_A_df.values)):
             lm = self.ls()
@@ -233,40 +276,52 @@ class BestSubsetSelectionFitTask(AllCombinationFitTask):
                 fit.unknown_spectrum_b.values[train_index]
             )
             predicted_b = lm.predict(fit.reference_spectra_A_df.values[test_index])
-            model_residuals[test_index, i] = fit.unknown_spectrum_b.values[test_index] - predicted_b
-            cp = np.sqrt(np.nansum(np.square(model_residuals[test_index, i])))
-            normalized_cp = cp / len(test_index)
-            normalized_C_p_list.append(normalized_cp)
-        return normalized_C_p_list, model_residuals
+            #residuals[test_index, i] = fit.unknown_spectrum_b.values[test_index] - predicted_b
+            #cp = np.sqrt(np.nansum(np.square(model_residuals[test_index, i])))
+            #normalized_cp = cp / len(test_index)
+            #normalized_C_p_list.append(normalized_cp)
+            yield predicted_b, train_index, test_index
 
     def plot_top_fits(self, spectrum, fit_results):
         log = logging.getLogger(name=self.__class__.__name__)
 
         figure_list = []
+
+        top_fit_per_component_count = {}
         for i, component_count in enumerate(fit_results.component_count_fit_table.keys()):
             f, ax = plt.subplots()
 
-            sorted_fits = fit_results.component_count_fit_table[component_count][:10]
+            pe_fits = [
+                fit
+                for fit
+                in fit_results.component_count_fit_table[component_count]
+                if hasattr(fit, 'median_C_p')
+            ]
+
+            sorted_fits = sorted(pe_fits, key=lambda fit: fit.median_C_p)[:10]
+            top_fit_per_component_count[component_count] = sorted_fits[0]
             ax.boxplot(
                 x=[fit_i.prediction_errors for fit_i in sorted_fits],
                 usermedians=[fit_i.median_C_p for fit_i in sorted_fits],
                 conf_intervals=[[fit_i.median_C_p_ci_lo, fit_i.median_C_p_ci_hi] for fit_i in sorted_fits],
                 notch=True
             )
+            ax.scatter(
+                x=range(1, len(sorted_fits)+1),
+                y=[fit_i.nss for fit_i in sorted_fits],
+                marker='x'
+            )
 
-            ax.set_title('Best Fits' + '\n' + spectrum.file_name + '\n' + '{} component(s)'.format(component_count))
-            ax.set_xlabel('best fits')
-            ax.set_ylabel('Normalized Prediction Error')
+            ax.set_title('Best {}-component Fits'.format(component_count) + '\n' + spectrum.file_name)
+            ax.set_xlabel('top 10 fits')
+            ax.set_ylabel('Prediction Error')
 
             self.add_date_time_footer(ax)
 
             f.tight_layout()
             figure_list.append(f)
 
-        for i, component_count in enumerate(fit_results.component_count_fit_table.keys()):
-            f, ax = plt.subplots()
-
-            sorted_fits = fit_results.component_count_fit_table[component_count][:10]
+            g, ax = plt.subplots()
             ax.errorbar(
                 y=[spectrum_fit.median_C_p for spectrum_fit in sorted_fits],
                 x=range(len(sorted_fits)),
@@ -274,40 +329,34 @@ class BestSubsetSelectionFitTask(AllCombinationFitTask):
                     [s.median_C_p - s.median_C_p_ci_lo for s in sorted_fits],
                     [s.median_C_p_ci_hi - s.median_C_p for s in sorted_fits]],
                 fmt='o')
-            ax.set_title('Best Fits' + '\n' + spectrum.file_name + '\n' + '{} component(s)'.format(component_count))
-            ax.set_xlabel('best fits')
-            ax.set_ylabel('Normalized Prediction Error')
+            ax.set_title('Best {}-component Fits'.format(component_count) + '\n' + spectrum.file_name)
+            ax.set_xlabel('top 10 fits')
+            ax.set_ylabel('Prediction Error')
             ax.grid()
 
             self.add_date_time_footer(ax)
 
-            f.tight_layout()
-            figure_list.append(f)
+            g.tight_layout()
+            figure_list.append(g)
 
-        return figure_list
+        f, ax = plt.subplots()
+        ax.boxplot(
+            x=[fit_i.prediction_errors for i, fit_i in sorted(top_fit_per_component_count.items())],
+            usermedians=[fit_i.median_C_p for i, fit_i in sorted(top_fit_per_component_count.items())],
+            conf_intervals=[
+                [fit_i.median_C_p_ci_lo, fit_i.median_C_p_ci_hi]
+                for i, fit_i
+                in sorted(top_fit_per_component_count.items())
+            ],
+            notch=True
+        )
+        ax.set_title('Best Fits' + '\n' + spectrum.file_name)
+        ax.set_xlabel('component count')
+        ax.set_ylabel('Prediction Error')
 
-    def plot_fit_path_old(self, spectrum, fit_results):
-        log = logging.getLogger(name=self.__class__.__name__)
+        self.add_date_time_footer(ax)
 
-        figure_list = []
-        for i, component_count in enumerate(fit_results.component_count_fit_table.keys()):
-            f, ax = plt.subplots()
-
-            sorted_fits = fit_results.component_count_fit_table[component_count][:10]
-            ax.errorbar(
-                y=[spectrum_fit.median_C_p for spectrum_fit in sorted_fits],
-                x=range(len(sorted_fits)),
-                yerr=[
-                    [s.median_C_p - s.median_C_p_ci_lo for s in sorted_fits],
-                    [s.median_C_p_ci_hi - s.median_C_p for s in sorted_fits]],
-                fmt='o')
-            ax.set_title('Fit Path' + '\n' + spectrum.file_name + '\n' + '{} component(s)'.format(component_count))
-            ax.set_xlabel('normalized median_C_p')
-            ax.set_ylabel('order')
-
-            self.add_date_time_footer(ax)
-
-            f.tight_layout()
-            figure_list.append(f)
+        f.tight_layout()
+        figure_list.append(f)
 
         return figure_list
